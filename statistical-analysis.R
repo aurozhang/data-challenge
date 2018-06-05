46aa2d1447365a74315fef8446425036bfcdea5b
Aurora Zhang <aurorazhang@Auroras-MacBook.local>
2018-06-05 16:03:19 -0400

    updated to include MAE and t-tests for predicting test scores

--- statistical-analysis.R
+++ statistical-analysis.R
@@ -36,3 +36,70 @@ summary(anovamodel)
 
 
 ## Day 2: Replicating results from paper predicting test scores
+
+# lapply(mcas[,-1], function(x) cor(x,mcas$MCAS))
+# finding the baseline MAE (for comparison purposes)
+
+d <- total_students_MCAS
+
+mcas <- d$MCAS
+baseline <- rep(mean(mcas), length(mcas))
+
+mae(baseline, mcas)
+
+cor(mcas, d$AveCorrect)
+
+
+# t-testing the significance of the difference
+# when using only corr, when using only affect, vs. using both performance and affect (all)
+
+
+# relevant dataframes 
+
+d.corr <- d[4]
+d.aff <- d[c(6:11)]
+d.all <- d[c(2:4, 6:11)]
+
+
+# linear models 
+
+lmcorr <- lm(MCAS ~ AveCorrect, data = d)
+lmaff <- lm(MCAS ~ AveResBored + AveResEngcon + AveResFrust + AveResOfftask + 
+              AveResGaming, data = d)
+lmall <- lm(MCAS ~ AveCorrect + AveKnow + AveCarelessness + AveResBored + 
+              AveResEngcon + AveResFrust + AveResOfftask + AveResGaming, data = d)
+#out <- CVlm(d,linear, m=5)
+#summary(out)
+
+
+# predictions and resulting errors 
+
+predictcorr <- predict(lmcorr, d.corr)
+predictaff <- predict(lmaff, d.aff)
+predictall <- predict(lmall, d.all)
+
+errorcorr <- abs(predictcorr - mcas)
+erroraff <- abs(predictaff - mcas)
+errorall <- abs(predictall - mcas)
+
+
+# the MAE results!!
+
+mean(errorcorr)
+mean(erroraff)
+mean(errorall)
+
+range(erroraff)
+
+
+# t-testing the errors
+
+t.test(errorcorr, errorall)
+t.test(erroraff, errorall)
+
+
+
+
+
+
+
